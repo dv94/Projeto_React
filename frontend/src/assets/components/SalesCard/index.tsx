ac39f9af747a7432d941ed66bc2406943cf7f234
dv94 <54440409+dv94@users.noreply.github.com>
2022-09-06 19:44:15 -0300

    useState2

--- frontend/src/assets/components/SalesCard/index.tsx
+++ frontend/src/assets/components/SalesCard/index.tsx
@@ -1,5 +1,5 @@
-import NotificationButton from '../NotificationButton'
-import './styles.css'
+import NotificationButton from '../NotificationButton';
+import './styles.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from 'react';
